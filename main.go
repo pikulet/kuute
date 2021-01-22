1986927a56f33fe0aa1d82ccc56524c6c0c70f44
joyce <jycyeo@yahoo.com.sg>
2021-01-22 17:05:15 +0800

    add etag

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
     "io/ioutil"
     "net/http"
 
+    "crypto/md5"
+
     "github.com/gin-gonic/gin"
 )
 
@@ -24,7 +26,6 @@ func main() {
 }
 
 func index (c *gin.Context) {
-    c.Header("Cache-Control", "no-cache")
     c.Redirect(http.StatusMovedPermanently, "https://github.com/pikulet/kuute")
     c.Abort()
 }
@@ -34,15 +35,18 @@ func getCounter (c *gin.Context) {
     count := kuuteDB.getCounter(name)
     img := getShieldsIOImage(count)
 
+    etag := fmt.Sprintf("%x", md5.Sum(img))
+    c.Header("Cache-Control", "no-cache")
     c.Header("Content-Type", "image/svg+xml;charset=utf-8")
-    c.String(http.StatusOK, img)
+    c.Header("ETag", etag)
+    c.String(http.StatusOK, string(img))
 }
 
 const (
     ShieldsIO string = "https://img.shields.io/badge/⭐ Views-%d-00bcc9"
 )
 
-func getShieldsIOImage(count int) string {
+func getShieldsIOImage(count int) []byte {
     site := fmt.Sprintf(ShieldsIO, count)
     resp, err := http.Get(site)
     if err != nil {
@@ -51,5 +55,5 @@ func getShieldsIOImage(count int) string {
     defer resp.Body.Close()
 
     body, err := ioutil.ReadAll(resp.Body)
-    return string(body)
+    return body
 }
